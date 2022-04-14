9fc4cef1d433252e01fd75488e4b5262c73ed5c7
Enrico Zannini <enricozannini93@gmail.com>
2022-04-14 18:53:02 +0200

    Implement firebase remote command via the Firebase binding library

---
+++ Integrations/Firebase/Tealium.RemoteCommands.Firebase.Shared/FirebaseRemoteCommand.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using Tealium;
+
+
+namespace Tealium.RemoteCommands.Firebase
+{
+    public abstract class FirebaseRemoteCommand : IRemoteCommand
+    {
+
+        public static readonly string KeySessionTimeout = "firebase_session_timeout_seconds";
+        public static readonly string KeyMinSeconds = "firebase_session_minimum_seconds";
+        public static readonly string KeyAnalyticsEnabled = "firebase_analytics_enabled";
+        // reserved for future use. log level can only be set
+        public static readonly string KeyLogLevel = "firebase_log_level";
+        public static readonly string KeyEventName = "firebase_event_name";
+        public static readonly string KeyEventParams = "firebase_event_params";
+        public static readonly string KeyScreenName = "firebase_screen_name";
+        public static readonly string KeyScreenClass = "firebase_screen_class";
+        public static readonly string KeyUserPropertyName = "firebase_property_name";
+        public static readonly string KeyUserPropertyValue = "firebase_property_value";
+        public static readonly string KeyUserId = "firebase_user_id";
+        public static readonly string KeyCommandName = "command_name";
+
+        protected static readonly Dictionary<string, string> eventsMap = new Dictionary<string, string>();
+        protected static readonly Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        public static readonly string DefaultCommandId = "firebaseAnalytics";
+        public static readonly string DefaultCommandDesc = "Tealium Firebase Analytics integration";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Tealium.RemoteCommands.Firebase.FirebaseRemoteCommand"/> class.
+        /// Pre-built Remote Command supporting Firebase Analytics for both iOS
+        /// and Android on Xamarin. Empty constructor will use the 
+        /// <see langword="static"/> String value of firebaseAnalytics as the
+        /// command id.
+        /// </summary>
+        public FirebaseRemoteCommand() : this(DefaultCommandId, DefaultCommandDesc)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Tealium.RemoteCommands.Firebase.FirebaseRemoteCommand"/> class.
+        /// Pre-built Remote Command supporting Firebase Analytics for both iOS
+        /// and Android on Xamarin. Supply a commmand id and description to 
+        /// override the defaults.
+        /// </summary>
+        /// <param name="commandId">Command identifier.</param>
+        /// <param name="description">Description.</param>
+        public FirebaseRemoteCommand(string commandId, string description)
+        {
+            this.CommandId = commandId ?? DefaultCommandId;
+            this.Description = description ?? DefaultCommandDesc;
+        }
+
+        public string CommandId { get; set; }
+
+        public string Description { get; set; }
+
+        public abstract string Path { get; }
+        public abstract string Url { get; }
+
+        /// <summary>
+        /// Handles the Remote Command response - this contains all the logic 
+        /// for executing the different functions Firebase Analytics.
+        /// A comma separated list of command names as set out in the <see cref="Commands"/>
+        /// class is required at the paylod key <see cref="KeyCommandName"/>
+        /// </summary>
+        /// <param name="response">Response.</param>
+        public void HandleResponse(IRemoteCommandResponse response)
+        {
+            String command = response.Payload.GetValueForKey<String>(KeyCommandName);
+            if (String.IsNullOrEmpty(command))
+            {
+                return;
+            }
+
+            String[] commandArray;
+            // split the commands into an array
+            commandArray = command.Split(",");
+
+            for (int j = 0, commandlen = commandArray.Length; j < commandlen; j++)
+            {
+                try
+                {
+                    command = commandArray[j];
+                    command = command.Trim();
+                    switch (command)
+                    {
+                        case Commands.Config:
+                            int? timeout = null;
+                            try
+                            {
+                                timeout = response.Payload.ContainsKey(KeySessionTimeout) == true ?
+                                            (int?)Int32.Parse(response.Payload.GetValueForKey<String>(KeySessionTimeout)) * 1000 : null;
+                            }
+                            catch { }
+
+                            int? minSeconds = null;
+                            try
+                            {
+                                minSeconds = response.Payload.ContainsKey(KeyMinSeconds) == true ?
+                                            (int?)Int32.Parse(response.Payload.GetValueForKey<String>(KeyMinSeconds)) * 1000 : null;
+                            }
+                            catch { }
+
+                            bool? analyticsEnabled = null;
+                            try
+                            {
+                                analyticsEnabled = response.Payload.GetValueForKey<String>(KeyAnalyticsEnabled) != null && response.Payload.GetValueForKey<String>(KeyAnalyticsEnabled) == "false" ?
+                                            (bool?)false : true;
+                            }
+                            catch { }
+
+                            Configure(timeout, minSeconds, analyticsEnabled);
+
+                            break;
+                        case Commands.LogEvent:
+                            String eventName = response.Payload.GetValueForKey<String>(KeyEventName);
+
+                            Dictionary<string, string> eventParams;
+                            if (response.Payload.ContainsKey(KeyEventParams))
+                            {
+                                try
+                                {
+                                    eventParams = response.Payload.GetValueForKey<Dictionary<string, string>>(KeyEventParams);
+                                }
+                                catch
+                                {
+                                    //fallback to empty dictionary.
+                                    eventParams = new Dictionary<string, string>();
+                                }
+                            }
+                            else
+                            {
+                                eventParams = new Dictionary<string, string>();
+                            }
+
+                            if (!IsNullOrNullString(eventName))
+                            {
+                                LogEvent(mapEventNames(eventName), eventParams);
+                            }
+                            break;
+                        case Commands.SetScreenName:
+                            String screenName = response.Payload.GetValueForKey<String>(KeyScreenName);
+                            String screenClass = response.Payload.GetValueForKey<String>(KeyScreenClass);
+                            if (!IsNullOrNullString(screenName) && !IsNullOrNullString(screenClass))
+                            {
+                                SetScreenName(screenName, screenClass);
+                            }
+                            break;
+                        case Commands.SetUserProperty:
+                            String propertyName = response.Payload.GetValueForKey<String>(KeyUserPropertyName);
+                            String propertyValue = response.Payload.GetValueForKey<String>(KeyUserPropertyValue);
+                            if (!IsNullOrNullString(propertyName) && !IsNullOrNullString(propertyValue))
+                            {
+                                SetUserProperty(propertyName, propertyValue);
+                            }
+                            break;
+                        case Commands.SetUserId:
+                            String userId = response.Payload.GetValueForKey<String>(KeyUserId);
+                            if (!IsNullOrNullString(userId))
+                            {
+                                SetUserId(userId);
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine("Error handling " + this.CommandId + ":" + command + " - " + e.Message);
+                    System.Diagnostics.Debug.WriteLine("Stack Trace: " + e.StackTrace);
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Helper method that returns a known event name or the <paramref name="eventName"/>
+        /// provided.
+        /// </summary>
+        /// <returns>The known even name, or provided event name</returns>
+        /// <param name="eventName">Event name.</param>
+        protected static String mapEventNames(String eventName)
+        {
+            return eventsMap[eventName] ?? eventName;
+        }
+
+        /// <summary>
+        /// Helper method that returns a known param name or the <paramref name="param"/>
+        /// provided.        
+        /// </summary>
+        /// <returns>The known parameter name, or provided param name.</returns>
+        /// <param name="param">Parameter.</param>
+        protected static String mapParams(String param)
+        {
+            return parameters[param] ?? param;
+        }
+
+        /// <summary>
+        /// Helper method to determine whether a string is null, or a null string.
+        /// The conversion from a <see langword="null"/> entry in an iOS disctionary
+        /// will end up casting the null to a string value "&lt;null&gt;"
+        /// Although this is iOS specific, we need to keep the handling in this class
+        /// so we can stop any unnecessary subclass method calls.
+        /// </summary>
+        /// <returns><c>true</c>, if null or null string was found, <c>false</c> otherwise.</returns>
+        /// <param name="value">Value.</param>
+        private bool IsNullOrNullString(string value)
+        {
+            //iOS null values in a NSDictionary come through as "<null>" string.
+            return value == null || value == "<null>";
+        }
+
+        /// <summary>
+        /// Configure Firebase Analytics with the specified sessionTimeoutDuration, 
+        /// minSessionDuration and analyticsEnabled.
+        /// </summary>
+        /// <param name="sessionTimeoutDuration">Session timeout duration.</param>
+        /// <param name="minSessionDuration">Minimum session duration.</param>
+        /// <param name="analyticsEnabled">Analytics enabled.</param>
+        protected abstract void Configure(int? sessionTimeoutDuration, int? minSessionDuration, bool? analyticsEnabled);
+
+        /// <summary>
+        /// Logs a Firebase event with the given <paramref name="eventName"/> and <paramref name="eventParams"/>.
+        /// </summary>
+        /// <param name="eventName">Event name.</param>
+        /// <param name="eventParams">Event parameters.</param>
+        protected abstract void LogEvent(String eventName, Dictionary<String, String> eventParams);
+
+        /// <summary>
+        /// Sets the <paramref name="screenName"/> and <paramref name="screenClass"/>.
+        /// </summary>
+        /// <param name="screenName">Screen name.</param>
+        /// <param name="screenClass">Screen class.</param>
+        protected abstract void SetScreenName(String screenName, String screenClass);
+
+        /// <summary>
+        /// Sets a property on the existing User, given by the supplied 
+        /// <paramref name="propertyName"/> and <paramref name="propertyValue"/>.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        /// <param name="propertyValue">Property value.</param>
+        protected abstract void SetUserProperty(String propertyName, String propertyValue);
+
+        /// <summary>
+        /// Sets the current user identifier.
+        /// </summary>
+        /// <param name="userId">User identifier.</param>
+        protected abstract void SetUserId(String userId);
+
+        public void Dispose()
+        {
+            //do nothing.
+        }
+
+        /// <summary>
+        /// Available Command names, this will be looked for in any Remote
+        /// Command payload at the key of <see cref="KeyCommandName"/>
+        /// </summary>
+        public static class Commands
+        {
+            public const String Config = "config";
+            public const String SetUserId = "setUserId";
+            public const String SetUserProperty = "setUserProperty";
+            public const String SetScreenName = "setScreenName";
+            public const String LogEvent = "logEvent";
+        }
+
+    }
+}
---
+++ Integrations/Firebase/Tealium.RemoteCommands.Firebase.iOS/FirebaseRemoteCommandIOS.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Tealium;
+
+using Firebase.Analytics;
+using Firebase.Core;
+
+namespace Tealium.RemoteCommands.Firebase.iOS
+{
+    public class FirebaseRemoteCommandIOS : FirebaseRemoteCommand, IRemoteCommand
+    {
+        //private static AnalyticsConfiguration fireAnalyticsConfig;
+        private static Configuration fireConfig = Configuration.SharedInstance;
+
+        public override string Path => throw new NotImplementedException();
+
+        public override string Url => throw new NotImplementedException();
+
+        static FirebaseRemoteCommandIOS()
+        {
+            eventsMap.Add("event_add_payment_info", EventNamesConstants.AddPaymentInfo);
+            eventsMap.Add("event_add_shipping_info", EventNamesConstants.AddShippingInfo);
+            eventsMap.Add("add_to_cart", EventNamesConstants.AddToCart);
+            eventsMap.Add("add_to_wishlist", EventNamesConstants.AddToWishlist);
+            eventsMap.Add("app_open", EventNamesConstants.AppOpen);
+            eventsMap.Add("event_begin_checkout", EventNamesConstants.BeginCheckout);
+            eventsMap.Add("event_campaign_details", EventNamesConstants.CampaignDetails);
+            eventsMap.Add("event_checkout_progress", EventNamesConstants.CheckoutProgress);
+            eventsMap.Add("event_earn_virtual_currency", EventNamesConstants.EarnVirtualCurrency);
+            eventsMap.Add("event_ecommerce_purchase", EventNamesConstants.EcommercePurchase);
+            eventsMap.Add("event_generate_lead", EventNamesConstants.GenerateLead);
+            eventsMap.Add("event_join_group", EventNamesConstants.JoinGroup);
+            eventsMap.Add("event_level_end", EventNamesConstants.LevelEnd);
+            eventsMap.Add("event_level_start", EventNamesConstants.LevelStart);
+            eventsMap.Add("event_level_up", EventNamesConstants.LevelUp);
+            eventsMap.Add("event_login", EventNamesConstants.Login);
+            eventsMap.Add("event_post_score", EventNamesConstants.PostScore);
+            eventsMap.Add("event_present_offer", EventNamesConstants.PresentOffer);
+            eventsMap.Add("event_ecommerce_purchase", EventNamesConstants.EcommercePurchase);
+            eventsMap.Add("event_purchase", EventNamesConstants.Purchase);
+            eventsMap.Add("event_purchase_refund", EventNamesConstants.PurchaseRefund);
+            eventsMap.Add("event_refund", EventNamesConstants.Refund);
+            eventsMap.Add("event_remove_cart", EventNamesConstants.RemoveFromCart);
+            eventsMap.Add("event_search", EventNamesConstants.Search);
+            eventsMap.Add("event_select_content", EventNamesConstants.SelectContent);
+            eventsMap.Add("event_select_item", EventNamesConstants.SelectItem);
+            eventsMap.Add("event_select_promotion", EventNamesConstants.SelectPromotion);
+            eventsMap.Add("event_set_checkout_option", EventNamesConstants.SetCheckoutOption);
+            eventsMap.Add("event_share", EventNamesConstants.Share);
+            eventsMap.Add("event_signup", EventNamesConstants.SignUp);
+            eventsMap.Add("event_spend_virtual_currency", EventNamesConstants.SpendVirtualCurrency);
+            eventsMap.Add("event_tutorial_begin", EventNamesConstants.TutorialBegin);
+            eventsMap.Add("event_tutorial_complete", EventNamesConstants.TutorialComplete);
+            eventsMap.Add("event_unlock_achievement", EventNamesConstants.UnlockAchievement);
+            eventsMap.Add("event_view_item", EventNamesConstants.ViewItem);
+            eventsMap.Add("event_view_item_list", EventNamesConstants.ViewItemList);
+            eventsMap.Add("event_view_search_results", EventNamesConstants.ViewSearchResults);
+
+
+            parameters.Add("param_achievement_id", ParameterNamesConstants.AchievementId);
+            parameters.Add("param_ad_network_click_id", ParameterNamesConstants.AdNetworkClickId);
+            parameters.Add("param_affiliation", ParameterNamesConstants.Affiliation);
+            parameters.Add("param_cp1", ParameterNamesConstants.Cp1);
+            parameters.Add("param_campaign", ParameterNamesConstants.Campaign);
+            parameters.Add("param_character", ParameterNamesConstants.Character);
+            parameters.Add("param_checkout_option", ParameterNamesConstants.CheckoutOption);
+            parameters.Add("param_checkout_step", ParameterNamesConstants.CheckoutStep);
+            parameters.Add("param_content", ParameterNamesConstants.Content);
+            parameters.Add("param_content_type", ParameterNamesConstants.ContentType);
+            parameters.Add("param_coupon", ParameterNamesConstants.Coupon);
+            parameters.Add("param_creative_name", ParameterNamesConstants.CreativeName);
+            parameters.Add("param_creative_slot", ParameterNamesConstants.CreativeSlot);
+            parameters.Add("param_currency", ParameterNamesConstants.Currency);
+            parameters.Add("param_destination", ParameterNamesConstants.Destination);
+            parameters.Add("param_discount", ParameterNamesConstants.Discount);
+            parameters.Add("param_end_date", ParameterNamesConstants.EndDate);
+            parameters.Add("param_extend_session", ParameterNamesConstants.ExtendSession);
+            parameters.Add("param_flight_number", ParameterNamesConstants.FlightNumber);
+            parameters.Add("param_group_id", ParameterNamesConstants.GroupId);
+            parameters.Add("param_index", ParameterNamesConstants.Index);
+            parameters.Add("param_item_brand", ParameterNamesConstants.ItemBrand);
+            parameters.Add("param_item_category", ParameterNamesConstants.ItemCategory);
+            parameters.Add("param_item_id", ParameterNamesConstants.ItemId);
+            parameters.Add("param_item_list", ParameterNamesConstants.ItemList);
+            parameters.Add("param_item_list_id", ParameterNamesConstants.ItemListId);
+            parameters.Add("param_item_list_name", ParameterNamesConstants.ItemListName);
+            parameters.Add("param_item_location_id", ParameterNamesConstants.ItemLocationId);
+            parameters.Add("param_item_name", ParameterNamesConstants.ItemName);
+            parameters.Add("param_item_variant", ParameterNamesConstants.ItemVariant);
+            parameters.Add("param_level", ParameterNamesConstants.Level);
+            parameters.Add("param_level_name", ParameterNamesConstants.LevelName);
+            parameters.Add("param_location", ParameterNamesConstants.Location);
+            parameters.Add("param_location_id", ParameterNamesConstants.LocationId);
+            parameters.Add("param_medium", ParameterNamesConstants.Medium);
+            parameters.Add("param_method", ParameterNamesConstants.Method);
+            parameters.Add("param_number_nights", ParameterNamesConstants.NumberOfNights);
+            parameters.Add("param_number_pax", ParameterNamesConstants.NumberOfPassengers);
+            parameters.Add("param_number_rooms", ParameterNamesConstants.NumberOfRooms);
+            parameters.Add("param_origin", ParameterNamesConstants.Origin);
+            parameters.Add("param_payment_type", ParameterNamesConstants.PaymentType);
+            parameters.Add("param_price", ParameterNamesConstants.Price);
+            parameters.Add("param_promotion_id", ParameterNamesConstants.PromotionId);
+            parameters.Add("param_promotion_name", ParameterNamesConstants.PromotionName);
+            parameters.Add("param_quantity", ParameterNamesConstants.Quantity);
+            parameters.Add("param_score", ParameterNamesConstants.Score);
+            parameters.Add("param_search_term", ParameterNamesConstants.SearchTerm);
+            parameters.Add("param_shipping", ParameterNamesConstants.Shipping);
+            parameters.Add("param_shipping_tier", ParameterNamesConstants.ShippingTier);
+            parameters.Add("param_signup_method", ParameterNamesConstants.SignUpMethod);
+            parameters.Add("param_source", ParameterNamesConstants.Source);
+            parameters.Add("param_start_date", ParameterNamesConstants.StartDate);
+            parameters.Add("param_success", ParameterNamesConstants.Success);
+            parameters.Add("param_tax", ParameterNamesConstants.Tax);
+            parameters.Add("param_term", ParameterNamesConstants.Term);
+            parameters.Add("param_transaction_id", ParameterNamesConstants.TransactionId);
+            parameters.Add("param_travel_class", ParameterNamesConstants.TravelClass);
+            parameters.Add("param_value", ParameterNamesConstants.Value);
+            parameters.Add("param_virtual_currency_name", ParameterNamesConstants.VirtualCurrencyName);
+            parameters.Add("param_user_signup_method", UserPropertyNamesConstants.SignUpMethod);
+
+        }
+
+
+        public FirebaseRemoteCommandIOS() : base()
+        {
+        }
+
+        protected override void Configure(int? sessionTimeoutDuration, int? minSessionDuration, bool? analyticsEnabled)
+        {
+            if (App.DefaultInstance == null)
+            {
+                //Can't call configure more than once... errors.
+                App.Configure();
+            }
+        }
+
+        protected override void LogEvent(string eventName, Dictionary<String, String> eventParams)
+        {
+            //if(IsNullOrNullString(eventName)) { return; }
+            Dictionary<object, object> dict = new Dictionary<object, object>();
+            foreach (var key in eventParams.Keys)
+            {
+                dict.Add(mapParams(key), eventParams.GetValueOrDefault(key));
+            }
+            Analytics.LogEvent(eventName, dict);
+        }
+
+        protected override void SetScreenName(string screenName, string screenClass)
+        {
+            //if(IsNullOrNullString(screenName) || IsNullOrNullString(screenClass)) { return; }
+            //Analytics.SetScreenNameAndClass(screenName, screenClass);
+            Analytics.LogEvent(EventNamesConstants.ScreenView, new Dictionary<object, object>
+            {
+                { ParameterNamesConstants.ScreenName, screenName },
+                { ParameterNamesConstants.ScreenClass, screenClass }
+            });
+        }
+
+        protected override void SetUserProperty(string propertyName, string propertyValue)
+        {
+            //if (IsNullOrNullString(propertyName) || IsNullOrNullString(propertyValue)) { return; }
+            Analytics.SetUserProperty(propertyName, propertyValue);
+        }
+
+        protected override void SetUserId(string userId)
+        {
+            Analytics.SetUserId(userId);
+        }
+    }
+}
